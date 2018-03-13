c4b87e643ea81e5513f9a65e38dee3f03cd22f7e
Lindsay Block <lindsay.block05@gmail.com>
2018-03-13 15:16:23 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import BlogForm from './BlogForm';
+import BlogList from './BlogList';
+import Footer from './Footer';
+
+const App =() => (
+  <div className="App">
+    <BlogForm />
+    <BlogList />
+    <Footer />
+  </div>
+);
+
+
+export default App;
